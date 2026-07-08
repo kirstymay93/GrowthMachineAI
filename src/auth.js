b6a0e0e54cef99c329344cc91b02062cc4371998
kirstymay93 <ndiscampaign@gmail.com>
2026-07-09 03:18:04 +1000

    Create auth.js
    Add React authentication helpers

---
+++ src/auth.js
@@ -0,0 +1,69 @@
+import { supabase } from "./supabase";
+
+
+export async function signUp(email,password){
+
+const {data,error}=
+
+await supabase.auth.signUp({
+
+email,
+
+password
+
+});
+
+
+if(error) throw error;
+
+
+return data;
+
+}
+
+
+
+export async function signIn(email,password){
+
+const {data,error}=
+
+await supabase.auth.signInWithPassword({
+
+email,
+
+password
+
+});
+
+
+if(error) throw error;
+
+
+return data;
+
+}
+
+
+
+export async function signOut(){
+
+await supabase.auth.signOut();
+
+}
+
+
+
+export async function getUser(){
+
+const {
+
+data:{user}
+
+}=
+
+await supabase.auth.getUser();
+
+
+return user;
+
+}
